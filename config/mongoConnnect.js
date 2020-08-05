031f5ce119d370f14771917df66984a672fa09e7
Ahmet <ahmetturgut@rotateknik.com.tr>
2020-08-05 10:51:40 +0300

    a

--- config/mongoConnnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-let db = 'mongodb://' + process.env.MONGOURL + '/communication'
-mongoose.connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-module.exports = mongoose
